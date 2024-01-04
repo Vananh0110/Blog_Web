34ce82e7d1c6b94bf3c58d62c293df95de87a346
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2024-01-04 23:01:59 +0700

    update route in App.js

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/guest/home/Home';
+import Login from './pages/guest/login/Login';
+import Register from './pages/guest/register/Register';
+import UserHome from './pages/user/home/UserHome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+
+          <Route path="/user/home" element={<UserHome/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- blog-backend/index.js
+++ blog-backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 var cors = require('cors');
+const blogRoutes = require('./src/blog/routes');
+const userRoutes = require('./src/user/routes');
+
 const app = express();
 const pool = require('./db');
 const port = 5000;
@@ -8,5 +11,10 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
+app.use('/blogs', blogRoutes);
+app.use('/auth', userRoutes)
 
+app.use('/', (req, res) => {
+  res.send('Hello');
+});
 app.listen(port, () => console.log(`app listening on port ${port}`));
