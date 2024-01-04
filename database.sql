929ed587d34f9f45bd200bc3c97edce950fc0ca5
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2024-01-04 08:42:10 +0700

    fix database

--- database.sql
+++ database.sql
@@ -13,7 +13,6 @@ CREATE TABLE users (
     user_name VARCHAR(50) NOT NULL,
     user_email VARCHAR(100) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
-    google_id VARCHAR(255),
     CONSTRAINT unique_email UNIQUE (user_email)
 );
 
