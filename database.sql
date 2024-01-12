c49b2ad167db89dddc1ee8a1fe1d41fc406602d7
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2024-01-12 16:28:21 +0700

    update database

--- database.sql
+++ database.sql
@@ -13,6 +13,8 @@ CREATE TABLE users (
     user_name VARCHAR(50) NOT NULL,
     user_email VARCHAR(100) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
+    avatar_url VARCHAR(255),
+    bio TEXT,
     CONSTRAINT unique_email UNIQUE (user_email)
 );
 
@@ -25,7 +27,8 @@ CREATE TABLE blogs (
     blog_id SERIAL PRIMARY KEY,
     user_id INT,
     title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
+    description TEXT NOT NULL,
+    information TEXT NOT NULL,
     category_id INT,
     imageUrl VARCHAR(255),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -42,3 +45,17 @@ CREATE TABLE comments (
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (blog_id) REFERENCES blogs(blog_id) ON DELETE CASCADE
 );
+
+INSERT INTO users (user_name, user_email, user_password)
+VALUES ('Pham Van Anh', 'phamvananh@gmail.com', '12345678');
+
+INSERT INTO categories (category_name)
+VALUES
+    ('art'),
+    ('technology'),
+    ('art'),
+    ('food');
+
+
+
+
